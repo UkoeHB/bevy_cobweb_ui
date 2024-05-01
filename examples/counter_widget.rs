fe297b84b68428eaa0081c4a4d0539aa34576dce
koe <ukoe@protonmail.com>
2024-05-01 13:49:29 -0500

    clean uup counter widget example

--- examples/counter_widget.rs
+++ examples/counter_widget.rs
@@ -1,6 +1,5 @@
 //! Demonstrates building a counter with a custom widget and theming.
 
-use bevy::ecs::system::EntityCommands;
 use bevy::prelude::*;
 use bevy::window::WindowTheme;
 use bevy_cobweb::prelude::*;
@@ -55,11 +54,11 @@ impl Counter
 
 /// Marker component for the counter theme.
 #[derive(Component)]
-struct CounterButtonTheme;
+struct CounterButton;
 
 //-------------------------------------------------------------------------------------------------------------------
 
-/// Pre-defined widget structure that can be reused and customized.
+/// Pre-defined widget structure that can be customized.
 #[derive(Default)]
 struct CounterWidget
 {
@@ -79,8 +78,6 @@ impl CounterWidget
     }
 
     /// Adds config adjustments to the button node on top of the default config.
-    ///
-    /// The loaded structure must match the default config's structure.
     fn button_config(mut self, config: LoadableRef) -> Self
     {
         self.button_config = config.into();
@@ -88,8 +85,6 @@ impl CounterWidget
     }
 
     /// Adds config adjustments to the text node on top of the default config.
-    ///
-    /// The loaded structure must match the default config's structure.
     fn text_config(mut self, config: LoadableRef) -> Self
     {
         self.text_config = config.into();
@@ -117,35 +112,39 @@ impl CounterWidget
         self
     }
 
+    /// Returns a reference to the base node of the counter widget (the button node).
     fn default_ref() -> LoadableRef
     {
-        LoadableRef::new("examples/advanced_counter.load.json", "counter_config")
+        LoadableRef::new("examples/counter_widget.load.json", "counter_widget")
     }
 
     /// Builds the widget on an entity.
-    //todo: use BuildWidget trait and extension method on UiBuilder?
-    fn build(self, ec: &mut EntityCommands)
+    fn build(self, builder: &mut UiBuilder<Entity>)
     {
-        let button_ref = self.button_config.unwrap_or_else(|| Self::default_ref());
-        let text_ref = self
-            .text_config
-            .unwrap_or_else(|| Self::default_ref().e("text"));
+        let (button_ref, text_ref) = match (self.button_config, self.text_config) {
+            (Some(b), Some(t)) => (b, t),
+            (Some(b), None) => (b, Self::default_ref().e("text")),
+            (None, Some(t)) => (Self::default_ref(), t),
+            (None, None) => {
+                // Avoid excess allocations in this case.
+                let b = Self::default_ref();
+                (b.clone(), b.e("text"))
+            }
+        };
         let pre_text = self.pre_text.unwrap_or_else(|| "Counter: ".into());
         let post_text = self.post_text.unwrap_or_else(|| "".into());
-        let entity = ec.id();
 
-        ec.commands()
-            .ui_builder(entity)
+        builder
             .load(button_ref, |button, _path| {
                 if let Some(theme) = self.theme {
                     button
                         .entity_commands()
-                        .load_theme::<CounterButtonTheme>(theme);
+                        .load_theme::<CounterButton>(theme);
                 }
 
                 let button_id = button.id();
                 button
-                    .insert(CounterButtonTheme)
+                    .insert(CounterButton)
                     .insert_reactive(Counter(0))
                     .on_pressed(Counter::increment(button_id));
 
@@ -162,42 +161,27 @@ impl CounterWidget
 
 fn build_ui(mut c: Commands)
 {
-    let file = LoadableRef::from_file("examples/advanced_counter.load.json");
+    let file = LoadableRef::from_file("examples/counter_widget.load.json");
 
-    c.ui_builder(UiRoot).load(file.e("root"), |root, path| {
+    c.ui_builder(UiRoot).load(file.e("root"), |mut root, _path| {
         root.entity_commands()
-            .load_theme::<CounterButtonTheme>(file.e("counter_theme"));
+            .load_theme::<CounterButton>(file.e("counter_theme"));
 
         // Default widget
-        CounterWidget::new().build(&mut root.entity_commands());
+        CounterWidget::new().build(&mut root);
 
-        // Widget with custom config
+        // Widget with custom text structure.
         CounterWidget::new()
-            .text_config(path.e("counter_config_text_small"))
+            .text_config(file.e("counter_widget_text_small"))
             .pre_text("Small: ")
-            .build(&mut root.entity_commands());
+            .build(&mut root);
 
         // Widget with theme adjustments
         CounterWidget::new()
-            .theme(path.e("counter_theme_bonus"))
-            .button_config(path.e("counter_config_button_bonus"))
+            .theme(file.e("counter_theme_flexible"))
+            .button_config(file.e("counter_widget_button_flexible"))
             .pre_text("Themed: ")
-            .build(&mut root.entity_commands());
-
-        // Manual counter
-        root.load(CounterWidget::default_ref(), |button, path| {
-            let button_id = button.id();
-            button
-                .insert(CounterButtonTheme)
-                .insert_reactive(Counter(0))
-                .on_pressed(Counter::increment(button_id));
-
-            button.load(path.e("text"), |text, _path| {
-                text.update_on(entity_mutation::<Counter>(button_id), |text_id| {
-                    Counter::write("Manual: ", "", button_id, text_id)
-                });
-            });
-        });
+            .build(&mut root);
     });
 }
 
@@ -222,8 +206,9 @@ fn main()
         }))
         .add_plugins(SickleUiPlugin)
         .add_plugins(CobwebUiPlugin)
-        .add_plugins(ComponentThemePlugin::<CounterButtonTheme>::new())
-        .add_load_sheet("examples/advanced_counter.load.json")
+        .add_plugins(ComponentThemePlugin::<CounterButton>::new())
+        .add_load_sheet("examples/counter_widget_config.load.json")
+        .add_load_sheet("examples/counter_widget.load.json")
         .add_systems(PreStartup, setup)
         .add_systems(Startup, build_ui)
         .run();
