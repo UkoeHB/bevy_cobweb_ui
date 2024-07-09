14c1e3e9710e8753d7c9d288cd36c195a3be12dc
koe <ukoe@protonmail.com>
2024-07-09 18:38:40 -0500

    move ui_2d (WIP) to ui_2d branch

--- src/ui_2d/parents/mod.rs
+++
@@ -1,7 +0,0 @@
-mod camera2d;
-mod parent;
-mod plugin;
-
-pub use camera2d::*;
-pub use parent::*;
-pub(crate) use plugin::*;
--- src/ui_2d/primitives/mod.rs
+++
@@ -1,7 +0,0 @@
-mod basic_image;
-mod block;
-mod plugin;
-
-pub use basic_image::*;
-pub use block::*;
-pub(crate) use plugin::*;
