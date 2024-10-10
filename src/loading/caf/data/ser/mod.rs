ae1b324d2cc7a55418dcfd3d4213a1324fcba154
koe <ukoe@protonmail.com>
2024-10-10 15:41:31 -0500

    rearrange

--- src/loading/caf/data/ser/mod.rs
+++ src/loading/caf/data/ser/mod.rs
@@ -1,5 +1,9 @@
+mod caf_instruction;
+mod caf_value;
 mod error;
 mod string;
 
+pub use caf_instruction::CafInstructionSerializer;
+pub use caf_value::CafValueSerializer;
 pub use error::*;
 pub(crate) use string::*;
