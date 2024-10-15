18af7b9ddd6fa6bfb9515531d8701401c1f557e7
koe <ukoe@protonmail.com>
2024-10-15 10:52:25 -0500

    move finite check into caf number value

--- src/loading/caf/raw_serializer.rs
+++ src/loading/caf/raw_serializer.rs
@@ -9,7 +9,9 @@ pub trait RawSerializer: std::io::Write
 {
     fn write_u128(&mut self, val: u128) -> Result<(), std::io::Error>;
     fn write_i128(&mut self, val: i128) -> Result<(), std::io::Error>;
+    /// Only finite numbers are passed in here.
     fn write_f64(&mut self, val: f64) -> Result<(), std::io::Error>;
+    /// Only finite numbers are passed in here.
     fn write_f32(&mut self, val: f32) -> Result<(), std::io::Error>;
     fn write_bytes(&mut self, bytes: &[u8]) -> Result<(), std::io::Error>;
 }
@@ -53,6 +55,8 @@ impl<'a> RawSerializer for DefaultRawSerializer<'a>
     }
     fn write_f64(&mut self, val: f64) -> Result<(), std::io::Error>
     {
+        debug_assert!(val.is_finite());
+
         // Try to write as integer.
         if self.simplify_fp {
             if val.is_finite() {
@@ -71,51 +75,30 @@ impl<'a> RawSerializer for DefaultRawSerializer<'a>
             }
         }
 
-        // Write NaN and inf/-inf with custom formatting.
-        if val.is_nan() {
-            write!(self, "nan")
-        } else if val.is_infinite() {
-            match val.is_sign_positive() {
-                true => write!(self, "inf"),
-                false => write!(self, "-inf"),
-            }
-        } else {
-            write!(self, "{:?}", val)
-        }
+        write!(self, "{:?}", val)
     }
     fn write_f32(&mut self, val: f32) -> Result<(), std::io::Error>
     {
+        debug_assert!(val.is_finite());
+
         // Try to write as integer.
         if self.simplify_fp {
-            if val.is_finite() {
-                let num = CafNumberValue::Float32(val);
-                // We only convert inside +/- 1e16 because the rust formatter only uses scientific notation for
-                // floats with magnitude >= 1e16 (it doesn't use scientific notation for ints).
-                if val.is_sign_positive() && val < 1e16 {
-                    if let Some(converted) = num.as_u128() {
-                        return self.write_u128(converted);
-                    }
-                } else if val.is_sign_negative() && val > -1e16 {
-                    if let Some(converted) = num.as_i128() {
-                        return self.write_i128(converted);
-                    }
+            let num = CafNumberValue::Float32(val);
+            // We only convert inside +/- 1e16 because the rust formatter only uses scientific notation for
+            // floats with magnitude >= 1e16 (it doesn't use scientific notation for ints).
+            if val.is_sign_positive() && val < 1e16 {
+                if let Some(converted) = num.as_u128() {
+                    return self.write_u128(converted);
+                }
+            } else if val.is_sign_negative() && val > -1e16 {
+                if let Some(converted) = num.as_i128() {
+                    return self.write_i128(converted);
                 }
             }
         }
 
-        // Write NaN and inf/-inf with custom formatting.
-        if val.is_nan() {
-            write!(self, "nan")
-        } else if val.is_infinite() {
-            match val.is_sign_positive() {
-                true => write!(self, "inf"),
-                false => write!(self, "-inf"),
-            }
-        } else {
-            write!(self, "{:?}", val)
-        }
+        write!(self, "{:?}", val)
     }
-
     fn write_bytes(&mut self, bytes: &[u8]) -> Result<(), std::io::Error>
     {
         self.write(bytes)?;
